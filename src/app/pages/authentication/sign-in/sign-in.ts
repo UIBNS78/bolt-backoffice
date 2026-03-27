0e6a282d60cac0f5477b5f62c78c258437ab409f
faniry <fdavidson@devout-services.com>
2026-03-27 11:12:59 +0300

    template sign in

--- src/app/pages/authentication/sign-in/sign-in.ts
+++ src/app/pages/authentication/sign-in/sign-in.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    InputTextModule,
+    ButtonModule
+  ],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
