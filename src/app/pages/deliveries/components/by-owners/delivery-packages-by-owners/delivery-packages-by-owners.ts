c375e4c468b87f5a6e6aaf89148b1d34ee6add10
faniry <fdavidson@devout-services.com>
2026-04-08 08:23:48 +0300

    search delivery packages

--- src/app/pages/deliveries/components/by-owners/delivery-packages-by-owners/delivery-packages-by-owners.ts
+++ src/app/pages/deliveries/components/by-owners/delivery-packages-by-owners/delivery-packages-by-owners.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, input, InputSignal, OnDestroy, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, inject, input, InputSignal, OnDestroy, Signal, signal, WritableSignal } from '@angular/core';
 import { Package } from '@shared/types/package';
 import { DeliveriesService } from 'app/pages/deliveries/deliveries-service';
 import { ButtonModule } from 'primeng/button';
@@ -6,7 +6,7 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { TooltipModule } from 'primeng/tooltip';
-import { finalize, Subject, takeUntil } from 'rxjs';
+import { debounceTime, distinctUntilChanged, finalize, Subject, takeUntil } from 'rxjs';
 import { DeliveryPackagesPlaceholder } from '../../delivery-packages-placeholder/delivery-packages-placeholder';
 import { TableModule } from 'primeng/table';
 import { NgxMaskPipe } from 'ngx-mask';
@@ -14,6 +14,8 @@ import { TagModule } from 'primeng/tag';
 import { PackageStatusPipe } from '@shared/pipes/package-status-pipe';
 import { PackageStatusSeverityPipe } from '@shared/pipes/package-status-severity-pipe';
 import { PackageStatusIconPipe } from '@shared/pipes/package-status-icon-pipe';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-delivery-packages-by-owners',
@@ -29,7 +31,8 @@ import { PackageStatusIconPipe } from '@shared/pipes/package-status-icon-pipe';
     TagModule,
     PackageStatusPipe,
     PackageStatusSeverityPipe,
-    PackageStatusIconPipe
+    PackageStatusIconPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './delivery-packages-by-owners.html',
   styleUrl: './delivery-packages-by-owners.css',
@@ -40,15 +43,33 @@ export class DeliveryPackagesByOwners implements OnDestroy {
 
   // vars
   private readonly unsubscribe$: Subject<void> = new Subject<void>();
+  protected searchControl: FormControl<string> = new FormControl({ value: "", disabled: true }, { nonNullable: true });
   deliveryId: InputSignal<number | null> = input<number | null>(null);
   protected isLoading: WritableSignal<boolean> = signal(false);
   protected packages: WritableSignal<Package[]> = signal([]);
+  protected searchValue: Signal<string> = toSignal(
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ),
+    { initialValue: "" }
+  );
+  protected filteredPackages: Signal<Package[]> = computed(() => {
+    if (this.searchValue() === "") return this.packages();
+
+    return this.packages().filter(p => {
+      return p.customer.toLowerCase().includes(this.searchValue().toLowerCase()) 
+        || p.deliveryMan.firstName.toLowerCase().includes(this.searchValue().toLowerCase())
+        || p.place.toLowerCase().includes(this.searchValue().toLowerCase())
+    });
+  });
 
   constructor() {
     effect(() => {
       if (!this.deliveryId()) {
         this.isLoading.set(false);
         this.packages.set([]);
+        this.searchControl.disable();
         return;
       }
 
@@ -67,6 +88,9 @@ export class DeliveryPackagesByOwners implements OnDestroy {
       takeUntil(this.unsubscribe$),
       finalize(() => this.isLoading.set(false))
     ).subscribe(packages => {
+      if (packages.length > 0) this.searchControl.enable();
+      else this.searchControl.disable();
+
       this.packages.set(packages);
     });
   }
