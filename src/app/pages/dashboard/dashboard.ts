55a1b4234336eda6a253bae8d97e172a4bbff52c
faniry <fdavidson@devout-services.com>
2026-04-23 15:43:41 +0300

    package status and top owner

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { DashboardAmountChart } from './components/dashboard-amount-chart/dashboard-amount-chart';
 import { DashboardCounts } from './components/dashboard-counts/dashboard-counts';
+import { DashboardPackageStatus } from './components/dashboard-package-status/dashboard-package-status';
+import { DashboardTopOwner } from './components/dashboard-top-owner/dashboard-top-owner';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     DashboardAmountChart,
-    DashboardCounts
+    DashboardCounts,
+    DashboardPackageStatus,
+    DashboardTopOwner
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
