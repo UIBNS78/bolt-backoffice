455c8429b14444ac8c19c6986a5b6ca8cc062914
faniry <fdavidson@devout-services.com>
2026-04-24 08:31:10 +0300

    move components folder

--- src/app/layouts/app-layout/app-layout.ts
+++ src/app/layouts/app-layout/app-layout.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideNav } from "./components/side-nav/side-nav";
+import { SideNav } from "../../components/side-nav/side-nav";
 import { DeliveryMenService } from 'app/pages/delivery-men/delivery-men-service';
 import { combineLatest, Subject, takeUntil } from 'rxjs';
 import { OwnersService } from 'app/pages/owners/owners-service';
