754706719eb8f18c9b4e531d95656462338129fe
faniry <fdavidson@devout-services.com>
2026-08-03 12:01:56 +0300

    add dm reward rates handler button

--- src/app/pages/delivery-men/components/placeholders/dm-reward-rates-placeholder/dm-reward-rates-placeholder.ts
+++ src/app/pages/delivery-men/components/placeholders/dm-reward-rates-placeholder/dm-reward-rates-placeholder.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
 import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-dm-reward-rates-placeholder',
   imports: [
-    SkeletonModule
+    SkeletonModule,
+    DividerModule
   ],
   templateUrl: './dm-reward-rates-placeholder.html',
   styleUrl: './dm-reward-rates-placeholder.css',
--- src/app/pages/delivery-men/components/rewards/rates/dm-reward-rates-handler/dm-reward-rates-handler.ts
+++ src/app/pages/delivery-men/components/rewards/rates/dm-reward-rates-handler/dm-reward-rates-handler.ts
@@ -1,9 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, input, InputSignal } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-dm-reward-rates-handler',
-  imports: [],
+  imports: [
+    SplitButtonModule,
+    TooltipModule
+  ],
   templateUrl: './dm-reward-rates-handler.html',
   styleUrl: './dm-reward-rates-handler.css',
 })
-export class DmRewardRatesHandler {}
+export class DmRewardRatesHandler {
+  // inputs
+  isLoading: InputSignal<boolean> = input.required();
+  
+  // vars
+  protected rewardActions: MenuItem[] = [
+    {
+      label: "Changer",
+      icon: 'pi pi-arrow-right-arrow-left',
+      items: [
+        {
+          label: "5 000 Ar",
+          icon: 'pi pi-star',
+          items: [
+            {
+              label: "Appliquer",
+              icon: "pi pi-check",
+              command: () => {}
+            },
+            {
+              label: "Modifier",
+              icon: "pi pi-pencil",
+              command: () => {}
+            },
+            {
+              label: "Supprimer",
+              icon: "pi pi-trash",
+              command: () => {}
+            }
+          ]
+        },
+        {
+          label: "10 000 Ar",
+          icon: 'pi pi-star',
+          items: [
+            {
+              label: "Appliquer",
+              icon: "pi pi-check",
+              command: () => {}
+            },
+            {
+              label: "Modifier",
+              icon: "pi pi-pencil",
+              command: () => {}
+            },
+            {
+              label: "Supprimer",
+              icon: "pi pi-trash",
+              command: () => {}
+            }
+          ]
+        },
+        {
+          label: "15 000 Ar",
+          icon: 'pi pi-check',
+          iconClass: "text-green-500!",
+          labelClass: "text-green-500",
+          disabled: true,
+          items: undefined
+        },
+      ]
+    },
+    { 
+      label: 'Nouveau', 
+      icon: 'pi pi-plus',
+      command: () => {}
+    },
+    {
+        separator: true
+    },
+    { 
+      label: 'Désactiver', 
+      icon: 'pi pi-ban',
+      iconClass: "text-red-500!",
+      labelClass: "text-red-500",
+      command: () => {}
+    },
+  ];
+}
